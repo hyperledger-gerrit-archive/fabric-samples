79a31d26e9d195ed5dd0eeb98eefd65027842dcf
Anil Ambati <aambati@us.ibm.com>
2017-09-21 08:53:58 -0400

    [FAB-6050] Added steps to query by a revoked user
    Steps include: revoking a user, generating a CRL,
    updating the CRL in the configuration block of the
    channel, and finally querying the chaincode using
    the revoked user credentials. The query will fail as
    it is invoked by a revoked user.
    
    Change-Id: I3b0f26d9b5a78475b6f42543b0e17458e9ce2a73
    Signed-off-by: Anil Ambati <aambati@us.ibm.com>

--- fabric-ca/scripts/env.sh
+++ fabric-ca/scripts/env.sh
@@ -75,6 +75,13 @@ export FABRIC_CA_CLIENT_ID_AFFILIATION=org1
 # Set to true to enable use of intermediate CAs
 USE_INTERMEDIATE_CA=true
 
+
+# Config block file path
+CONFIG_BLOCK_FILE=/tmp/config_block.pb
+
+# Update config block payload file path
+CONFIG_UPDATE_ENVELOPE_FILE=/tmp/config_update_as_envelope.pb
+
 # initOrgVars <ORG>
 function initOrgVars {
    if [ $# -ne 1 ]; then
@@ -228,7 +235,7 @@ function switchToAdminIdentity {
 function switchToUserIdentity {
    export FABRIC_CA_CLIENT_HOME=/etc/hyperledger/fabric/orgs/$ORG/user
    export CORE_PEER_MSPCONFIGPATH=$FABRIC_CA_CLIENT_HOME/msp
-   if [ ! -d $FABRIC_CA_CLIENT_HOME ]; then
+   if [ ! -d "$FABRIC_CA_CLIENT_HOME" ]; then
       dowait "$CA_NAME to start" 10 $CA_LOGFILE $CA_CHAINFILE
       log "Enrolling user for organization $ORG with home directory $FABRIC_CA_CLIENT_HOME ..."
       export FABRIC_CA_CLIENT_TLS_CERTFILES=$CA_CHAINFILE
@@ -240,6 +247,25 @@ function switchToUserIdentity {
          cp $ORG_ADMIN_HOME/msp/signcerts/* $ACDIR
       fi
    fi
+}
+
+# Revokes the fabric user
+function revokeFabricUser {
+   switchToAdminIdentity
+   export  FABRIC_CA_CLIENT_HOME=$ORG_ADMIN_HOME
+   logr "Revoking the user '$USER_NAME' of the organization '$ORG' with Fabric CA Client home directory set to $FABRIC_CA_CLIENT_HOME ..."
+   export FABRIC_CA_CLIENT_TLS_CERTFILES=$CA_CHAINFILE
+   fabric-ca-client revoke -d --revoke.name $USER_NAME
+}
+
+# Generates a CRL that contains serial numbers of all revoked enrollment certificates.
+# The generated CRL is placed in the crls folder of the admin's MSP
+function generateCRL {
+   switchToAdminIdentity
+   export FABRIC_CA_CLIENT_HOME=$ORG_ADMIN_HOME
+   logr "Generating CRL for the organization '$ORG' with Fabric CA Client home directory set to $FABRIC_CA_CLIENT_HOME ..."
+   export FABRIC_CA_CLIENT_TLS_CERTFILES=$CA_CHAINFILE
+   fabric-ca-client gencrl -d
 }
 
 # Copy the org's admin cert into some target MSP directory
