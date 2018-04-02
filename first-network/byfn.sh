af5a4156009ae7a4f18bad5424b3a411973272c7
Christopher Ferris <chrisfer@us.ibm.com>
2018-04-02 13:56:05 -0400

    FAB-9294 eliminate excess noise in BYFN
    eliminate the noise of echoing the environment
    variables for each command and add -v option to
    enable the noise for debug purposes for byfn.sh
    and eyfn.sh in first-network sample
    
    Change-Id: I46c377360efbab598fd37c0a31b29a119b99173e
    Signed-off-by: Christopher Ferris <chrisfer@us.ibm.com>

--- first-network/byfn.sh
+++ first-network/byfn.sh
@@ -30,6 +30,7 @@
 # this may be commented out to resolve installed version of tools if desired
 export PATH=${PWD}/../bin:${PWD}:$PATH
 export FABRIC_CFG_PATH=${PWD}
+export VERBOSE=false
 
 # Print the usage message
 function printHelp () {
@@ -49,6 +50,7 @@ function printHelp () {
   echo "    -s <dbtype> - the database backend to use: goleveldb (default) or couchdb"
   echo "    -l <language> - the chaincode language: golang (default) or node"
   echo "    -i <imagetag> - the tag to be used to launch the network (defaults to \"latest\")"
+  echo "    -v - verbose mode"
   echo
   echo "Typically, one would first generate the required certificates and "
   echo "genesis block, then bring up the network. e.g.:"
@@ -87,7 +89,7 @@ function askProceed () {
 # Obtain CONTAINER_IDS and remove them
 # TODO Might want to make this optional - could clear other containers
 function clearContainers () {
-  CONTAINER_IDS=$(docker ps -a |awk '($2 ~ /dev-peer.*.mycc.*/) {print $1}')
+  CONTAINER_IDS=$(docker ps -aq)
   if [ -z "$CONTAINER_IDS" -o "$CONTAINER_IDS" == " " ]; then
     echo "---- No containers available for deletion ----"
   else
@@ -99,7 +101,7 @@ function clearContainers () {
 # specifically the following images are often left behind:
 # TODO list generated image naming patterns
 function removeUnwantedImages() {
-  DOCKER_IMAGE_IDS=$(docker images|awk '($1 ~ /dev-peer.*.mycc.*/) {print $3}')
+  DOCKER_IMAGE_IDS=$(docker images | grep "dev\|none\|test-vp\|peer[0-9]-" | awk '{print $3}')
   if [ -z "$DOCKER_IMAGE_IDS" -o "$DOCKER_IMAGE_IDS" == " " ]; then
     echo "---- No images available for deletion ----"
   else
@@ -163,7 +165,7 @@ function networkUp () {
     exit 1
   fi
   # now run the end to end script
-  docker exec cli scripts/script.sh $CHANNEL_NAME $CLI_DELAY $LANGUAGE $CLI_TIMEOUT
+  docker exec cli scripts/script.sh $CHANNEL_NAME $CLI_DELAY $LANGUAGE $CLI_TIMEOUT $VERBOSE
   if [ $? -ne 0 ]; then
     echo "ERROR !!!! Test failed"
     exit 1
@@ -222,7 +224,7 @@ function upgradeNetwork () {
     docker-compose $COMPOSE_FILES up -d --no-deps $PEER
   done
 
-  docker exec cli scripts/upgrade_to_v11.sh $CHANNEL_NAME $CLI_DELAY $LANGUAGE $CLI_TIMEOUT
+  docker exec cli scripts/upgrade_to_v11.sh $CHANNEL_NAME $CLI_DELAY $LANGUAGE $CLI_TIMEOUT $VERBOSE
   if [ $? -ne 0 ]; then
     echo "ERROR !!!! Test failed"
     exit 1
@@ -465,7 +467,7 @@ else
   exit 1
 fi
 
-while getopts "h?m:c:t:d:f:s:l:i:" opt; do
+while getopts "h?m:c:t:d:f:s:l:i:v" opt; do
   case "$opt" in
     h|\?)
       printHelp
@@ -484,6 +486,8 @@ while getopts "h?m:c:t:d:f:s:l:i:" opt; do
     l)  LANGUAGE=$OPTARG
     ;;
     i)  IMAGETAG=`uname -m`"-"$OPTARG
+    ;;
+    v)  VERBOSE=true
     ;;
   esac
 done
