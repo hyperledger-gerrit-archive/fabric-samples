0fceb3ff1828c42a7f74adea0afd5d004d65ae6f
Naga Pemmara <naga.pemmaraju@ibm.com>
2018-06-20 10:12:04 -0400

    [FAB-10732] BYFN upgrade to v1.2
    This patch set includes changes for demonstrating
    byfn network sample upgrade from v1.1 to v1.2
    
    Change-Id: Ib0e5243e7ec280861c13feab0a23dc3566674f4c
    Signed-off-by: Naga Pemmara <naga.pemmaraju@ibm.com>

---
+++ first-network/scripts/pull-docker-images.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -e
+#
+# Copyright IBM Corp. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+############################################
+# Pull "stable" docker images from nexus3
+# Tag it as $ARCH-$VERSION (BASE_VERSION)
+#############################################
+
+export ORG_NAME=hyperledger/fabric
+export NEXUS_URL=nexus3.hyperledger.org:10001
+export VERSION=1.2.0
+export SUFFIX=stable
+export OS_VER=$(uname -s | tr '[:upper:]' '[:lower:]')
+echo "---------> OS_VER" $OS_VER
+export ARCH=$(go env GOARCH)
+if [ "$ARCH" = "amd64" ]; then
+  ARCH=amd64
+else
+  ARCH=$(uname -m)
+fi
+
+for IMAGES in peer orderer ccenv tools ca; do
+  docker pull $NEXUS_URL/$ORG_NAME-$IMAGES:$ARCH-$VERSION-$SUFFIX
+  docker tag $NEXUS_URL/$ORG_NAME-$IMAGES:$ARCH-$VERSION-$SUFFIX $ORG_NAME-$IMAGES:$ARCH-$VERSION
+  docker tag $NEXUS_URL/$ORG_NAME-$IMAGES:$ARCH-$VERSION-$SUFFIX $ORG_NAME-$IMAGES:latest
+  docker rmi -f $NEXUS_URL/$ORG_NAME-$IMAGES:$ARCH-$VERSION-$SUFFIX
+done
+
+MVN_METADATA=$(echo "https://nexus.hyperledger.org/content/repositories/releases/org/hyperledger/fabric/hyperledger-fabric-stable/maven-metadata.xml")
+curl -L "$MVN_METADATA" >maven-metadata.xml
+RELEASE_TAG=$(cat maven-metadata.xml | grep release)
+export COMMIT=$(echo $RELEASE_TAG | awk -F - '{ print $4 }' | cut -d "<" -f1)
+echo "---------> COMMIT:" $COMMIT
+curl https://nexus.hyperledger.org/content/repositories/releases/org/hyperledger/fabric/hyperledger-fabric-stable/$OS_VER-$ARCH.$VERSION-$SUFFIX-$COMMIT/hyperledger-fabric-stable-$OS_VER-$ARCH.$VERSION-$SUFFIX-$COMMIT.tar.gz | tar xz
+echo "---------> Working DIR " $PWD
+ls -l bin/
