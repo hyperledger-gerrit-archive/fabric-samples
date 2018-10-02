d5ed6ec04f7a127e8b33c275049f64c147cd27a8
Simon Stone <sstone1@uk.ibm.com>
2018-10-02 10:32:07 +0100

    [FAB-12219] Rename node to javascript for all samples
    Prepare the samples repository for a set of TypeScript samples by
    renaming the existing "node" sample directories to "javascript".
    
    The naming is wrong anyway; "node" is the name of the runtime
    environment, not the name of the programming language used.
    
    Changes affect the following samples:
     - balance-transfer
     - chaincode_example02
     - fabcar
     - marbles02
     - first-network
    
    Change-Id: I460f549b8ec47f9fcc838e1f10d3dfacaa74eefb
    Signed-off-by: Simon Stone <sstone1@uk.ibm.com>

--- first-network/eyfn.sh
+++ first-network/eyfn.sh
@@ -31,7 +31,7 @@ function printHelp () {
   echo "    -d <delay> - delay duration in seconds (defaults to 3)"
   echo "    -f <docker-compose-file> - specify which docker-compose file use (defaults to docker-compose-cli.yaml)"
   echo "    -s <dbtype> - the database backend to use: goleveldb (default) or couchdb"
-  echo "    -l <language> - the chaincode language: golang (default) or node"
+  echo "    -l <language> - the programming language of the chaincode to deploy: golang (default), javascript, or java"
   echo "    -i <imagetag> - the tag to be used to launch the network (defaults to \"latest\")"
   echo "    -v - verbose mode"
   echo
@@ -40,7 +40,7 @@ function printHelp () {
   echo
   echo "	eyfn.sh generate -c mychannel"
   echo "	eyfn.sh up -c mychannel -s couchdb"
-  echo "	eyfn.sh up -l node"
+  echo "	eyfn.sh up -l javascript"
   echo "	eyfn.sh down -c mychannel"
   echo
   echo "Taking all defaults:"
@@ -112,7 +112,7 @@ function networkUp () {
   echo "###############################################################"
   echo "############### Have Org3 peers join network ##################"
   echo "###############################################################"
-  docker exec Org3cli ./scripts/step2org3.sh $CHANNEL_NAME $CLI_DELAY $LANGUAGE $CLI_TIMEOUT $VERBOSE
+  docker exec Org3cli ./scripts/step2org3.sh $CHANNEL_NAME $CLI_DELAY $CC_SRC_LANGUAGE $CLI_TIMEOUT $VERBOSE
   if [ $? -ne 0 ]; then
     echo "ERROR !!!! Unable to have Org3 peers join network"
     exit 1
@@ -121,13 +121,13 @@ function networkUp () {
   echo "###############################################################"
   echo "##### Upgrade chaincode to have Org3 peers on the network #####"
   echo "###############################################################"
-  docker exec cli ./scripts/step3org3.sh $CHANNEL_NAME $CLI_DELAY $LANGUAGE $CLI_TIMEOUT $VERBOSE
+  docker exec cli ./scripts/step3org3.sh $CHANNEL_NAME $CLI_DELAY $CC_SRC_LANGUAGE $CLI_TIMEOUT $VERBOSE
   if [ $? -ne 0 ]; then
     echo "ERROR !!!! Unable to add Org3 peers on network"
     exit 1
   fi
   # finish by running the test
-  docker exec Org3cli ./scripts/testorg3.sh $CHANNEL_NAME $CLI_DELAY $LANGUAGE $CLI_TIMEOUT $VERBOSE
+  docker exec Org3cli ./scripts/testorg3.sh $CHANNEL_NAME $CLI_DELAY $CC_SRC_LANGUAGE $CLI_TIMEOUT $VERBOSE
   if [ $? -ne 0 ]; then
     echo "ERROR !!!! Unable to run test"
     exit 1
@@ -157,7 +157,7 @@ function createConfigTx () {
   echo "###############################################################"
   echo "####### Generate and submit config tx to add Org3 #############"
   echo "###############################################################"
-  docker exec cli scripts/step1org3.sh $CHANNEL_NAME $CLI_DELAY $LANGUAGE $CLI_TIMEOUT $VERBOSE
+  docker exec cli scripts/step1org3.sh $CHANNEL_NAME $CLI_DELAY $CC_SRC_LANGUAGE $CLI_TIMEOUT $VERBOSE
   if [ $? -ne 0 ]; then
     echo "ERROR !!!! Unable to create config tx"
     exit 1
@@ -246,7 +246,7 @@ COMPOSE_FILE_ORG3=docker-compose-org3.yaml
 #
 COMPOSE_FILE_COUCH_ORG3=docker-compose-couch-org3.yaml
 # use golang as the default language for chaincode
-LANGUAGE=golang
+CC_SRC_LANGUAGE=golang
 # default image tag
 IMAGETAG="latest"
 
@@ -284,7 +284,7 @@ while getopts "h?c:t:d:f:s:l:i:v" opt; do
     ;;
     s)  IF_COUCHDB=$OPTARG
     ;;
-    l)  LANGUAGE=$OPTARG
+    l)  CC_SRC_LANGUAGE=$OPTARG
     ;;
     i)  IMAGETAG=$OPTARG
     ;;
