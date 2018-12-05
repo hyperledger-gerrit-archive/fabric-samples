f26005b1a564a818162ad8175971a1b6a86592df
Matthew B White <whitemat@uk.ibm.com>
2018-12-05 15:42:23 +0000

    [FAB-13175] Metadata add to samples
    Change-Id: I4ec8290fcab5bc76616793242f8230269ae91c69
    Signed-off-by: Matthew B White <whitemat@uk.ibm.com>

--- chaincode/fabcar/typescript/src/car.ts
+++ chaincode/fabcar/typescript/src/car.ts
@@ -1,11 +1,23 @@
 /*
  * SPDX-License-Identifier: Apache-2.0
  */
+import { Object, Property } from 'fabric-contract-api';
 
+@Object('Car')
 export class Car {
+
+    @Property('type')
     public docType?: string;
+
+    @Property('color')
     public color: string;
+
+    @Property('make')
     public make: string;
+
+    @Property('model')
     public model: string;
+
+    @Property('owner')
     public owner: string;
 }
