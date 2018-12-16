af83de1fcd90f46c3c1abeec1adcff494977cc24
Sambhav Nidamarty <sambhavdutt@gmail.com>
2018-12-16 16:53:14 -0500

    WIP Update scripts to pull the fabric, ca binaries
    Change-Id: I0a310385285b1c086a6cfef0f7cb1c1906ed8351
    Signed-off-by: Sambhav Nidamarty <sambhavdutt@gmail.com>

--- scripts/Jenkins_Scripts/byfn_eyfn.sh
+++ scripts/Jenkins_Scripts/byfn_eyfn.sh
@@ -8,25 +8,37 @@
 # docker container list
 CONTAINER_LIST=(peer0.org1 peer1.org1 peer0.org2 peer1.org2 peer0.org3 peer1.org3 orderer)
 COUCHDB_CONTAINER_LIST=(couchdb0 couchdb1 couchdb2 couchdb3 couchdb4 couchdb5)
+for repo in fabric fabric-ca; do
+    echo "======== PULL $repo BINARIES ========"
+    echo
+    # Set Nexus Snapshot URL
+    NEXUS_URL=https://nexus.hyperledger.org/content/repositories/snapshots/org/hyperledger/$repo/hyperledger-$repo-$RELEASE_VERSION/$ARCH.$RELEASE_VERSION-SNAPSHOT
 
-MARCH=$(uname -s|tr '[:upper:]' '[:lower:]')
-echo "-----------> MARCH" $MARCH
-echo "-----------> PROJECT_VERSION:" $PROJECT_VERSION
-# Download latest binaries from nexus2
-MVN_METADATA=$(echo "https://nexus.hyperledger.org/content/repositories/releases/org/hyperledger/fabric/hyperledger-fabric-$PROJECT_VERSION/maven-metadata.xml")
-curl -L "$MVN_METADATA" > maven-metadata.xml
-RELEASE_TAG=$(cat maven-metadata.xml | grep release)
-COMMIT=$(echo $RELEASE_TAG | awk -F - '{ print $4 }' | cut -d "<" -f1)
-echo "-----------> COMMIT = $COMMIT"
-cd $BASE_FOLDER/fabric-samples || exit
-curl https://nexus.hyperledger.org/content/repositories/releases/org/hyperledger/fabric/hyperledger-fabric-$PROJECT_VERSION/$MARCH-$ARCH.$PROJECT_VERSION-$COMMIT/hyperledger-fabric-$PROJECT_VERSION-$MARCH-$ARCH.$PROJECT_VERSION-$COMMIT.tar.gz | tar xz
+    # Download the maven-metadata.xml file
+    curl $NEXUS_URL/maven-metadata.xml > maven-metadata.xml
+    if grep -q "not found in local storage of repository" "maven-metadata.xml"; then
+        echo  "FAILED: Unable to download from $NEXUS_URL"
+    else
+        # Set latest tar file to the VERSION
+        VERSION=$(grep value maven-metadata.xml | sort -u | cut -d "<" -f2|cut -d ">" -f2)
+        echo "Version: $VERSION..."
 
-if [ $? -ne 0 ]; then
-   echo -e "\033[31m FAILED to download binaries" "\033[0m"
-   exit 1
-fi
+        # Download tar.gz file and extract it
+        cd $BASE_FOLDER/fabric-samples || exit
+        mkdir -p $BASE_FOLDER/fabric-samples/bin
+        curl $NEXUS_URL/hyperledger-$repo-$RELEASE_VERSION-$VERSION.tar.gz | tar xz
+         if [ $? -ne 0 ]; then
+            echo -e "\033[31m FAILED to download binaries" "\033[0m"
+            exit 1
+         fi
+        rm hyperledger-$repo-*.tar.gz
+        rm -f maven-metadata.xml
+        echo "Finished pulling $repo..."
+        echo
+    fi
+done
 
-cd first-network || exit
+cd $BASE_FOLDER/fabric-samples/first-network || exit
 export PATH=gopath/src/github.com/hyperledger/fabric-samples/bin:$PATH
 
 logs() {
