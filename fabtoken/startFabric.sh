439415dc2e1c0010427259cd5102c63904a72ff2
wenjian3 <wenjianq@gmail.com>
2019-03-21 20:04:46 -0400

    [FABN-1184] Implement fabtoken sample app
    - implement sample app to issue, transfer, redeem and list tokens
    - create v2.0 configtx.yaml and tokenchannel.tx
    - recreate crypto-config with multiple users
    - recreate genesis.block and channel.tx
    - add fabtoken/README.md
    
    Change-Id: I8d270c95b29e4af9c432fb05e7e8dc6be7bbc069
    Signed-off-by: Wenjian Qiao <wenjianq@gmail.com>

--- fabtoken/startFabric.sh
+++ fabtoken/startFabric.sh
@@ -10,29 +10,6 @@ set -e
 # don't rewrite paths for Windows Git Bash users
 export MSYS_NO_PATHCONV=1
 starttime=$(date +%s)
-CC_SRC_LANGUAGE=${1:-"go"}
-CC_SRC_LANGUAGE=`echo "$CC_SRC_LANGUAGE" | tr [:upper:] [:lower:]`
-if [ "$CC_SRC_LANGUAGE" = "go" -o "$CC_SRC_LANGUAGE" = "golang"  ]; then
-	CC_RUNTIME_LANGUAGE=golang
-	CC_SRC_PATH=github.com/fabcar/go
-elif [ "$CC_SRC_LANGUAGE" = "javascript" ]; then
-	CC_RUNTIME_LANGUAGE=node # chaincode runtime language is node.js
-	CC_SRC_PATH=/opt/gopath/src/github.com/fabcar/javascript
-elif [ "$CC_SRC_LANGUAGE" = "typescript" ]; then
-	CC_RUNTIME_LANGUAGE=node # chaincode runtime language is node.js
-	CC_SRC_PATH=/opt/gopath/src/github.com/fabcar/typescript
-	echo Compiling TypeScript code into JavaScript ...
-	pushd ../chaincode/fabcar/typescript
-	npm install
-	npm run build
-	popd
-	echo Finished compiling TypeScript code into JavaScript
-else
-	echo The chaincode language ${CC_SRC_LANGUAGE} is not supported by this script
-	echo Supported chaincode languages are: go, javascript, and typescript
-	exit 1
-fi
-
 
 # launch network; create channel and join peer to channel
 cd ../basic-network
@@ -50,10 +27,37 @@ Next, use the FabToken application to interact with the Fabric network.
   Next, install all required packages:
     npm install
 
-  Then run the fabtoken application to perform the token operations
+  Then run the fabtoken application to perform the token operations.
 
-    node fabtoken issue user1 100
-    node fabtoken move user1 user2 50
-    node fabtoken query user1
+    node fabtoken issue <username> <token_type> <quantity>
+      - example: node fabtoken issue user1 USD 100
+      - issue a token to user1 with type of "USD" and quantity of 100
+    node fabtoken list <username>
+      - example: node fabtoken list user1
+      - user1 lists his tokens
+      - a user can only list, transfer, and redeem tokens owned by himself.
+      - the list operation returns a list of tokens and each token has a tx_id and index.
+        [ { id:
+               { tx_id: 'ab5670d3b20b6247b17a342dd2c5c4416f79db95c168beccb7d32b3dd382e5a5',
+                 index: 0 },
+            type: 'EURO',
+            quantity: '200' },
+          { id:
+               { tx_id: 'c9b1211d9ad809e6ee1b542de6886d8d1d9e1c938d88eff23a3ddb4e8c080e4d',
+                 index: 0 },
+            type: 'USD',
+            quantity: '100' }]
+      - select a token to transfer or redeem and pass "tx_id" and "index" as input parameters
+    node fabtoken transfer <from_user> <to_user> <transfer_quantity> <remaining_quantity> <tx_id> <index>
+      - example: node fabtoken transfer user1 user2 30 70 c9b1211d9ad809e6ee1b542de6886d8d1d9e1c938d88eff23a3ddb4e8c080e4d 0
+      - user1 transfers to user2 "30" quantity of the token as specified by <tx_id> and <index>
+      - <tx_id> and <index> are the "tx_id" and "index" returned from the list operation
+      - "30" is the quantity you want to transfer and "70" is the remaining quantity after transfer.
+        Both numbers are required and they must be added up to the same quantity as the original token.
+        Otherwise, the transaction will be invalidated.
+    node fabtoken redeem <username> <redeem_quantity> <tx_id> <index>
+      - example: node fabtoken redeem user2 10 477c7bf2002814497c228fd8cbc4d80c8b7f1602b2c17ffadb6cf7e5783fa47a 0
+      - user2 redeems "10" quantity of the token as specified by <tx_id> and <index>
+      - <tx_id> and <index> are the "tx_id" and "index" returned from the list operation
 
 EOF
