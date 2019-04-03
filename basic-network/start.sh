03a1e59a2470f9f8f1e1a8feef4d8a4d7e467de8
Alessandro Sorniotti <ale.linux@sopit.net>
2019-04-03 10:02:14 +0200

    [FAB-15022] Basic-network support for new lifecycle
    This change set upgrades config files, scripts and fixtures for the
    basic-network sample.
    
    Change-Id: I201a98e3bf4d9717741539f572719aa687f4bb63
    Signed-off-by: Alessandro Sorniotti <ale.linux@sopit.net>

--- basic-network/start.sh
+++ basic-network/start.sh
@@ -5,18 +5,18 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 # Exit on first error, print all commands.
-set -ev
+set -evx
 
 # don't rewrite paths for Windows Git Bash users
 export MSYS_NO_PATHCONV=1
 
 docker-compose -f docker-compose.yml down
 
-docker-compose -f docker-compose.yml up -d ca.example.com orderer.example.com peer0.org1.example.com couchdb
+docker-compose -f docker-compose.yml up -d ca.example.com orderer.example.com peer0.org1.example.com couchdb cli
 
 # wait for Hyperledger Fabric to start
 # incase of errors when running later commands, issue export FABRIC_START_TIMEOUT=<larger number>
-export FABRIC_START_TIMEOUT=10
+export FABRIC_START_TIMEOUT=2
 #echo ${FABRIC_START_TIMEOUT}
 sleep ${FABRIC_START_TIMEOUT}
 
