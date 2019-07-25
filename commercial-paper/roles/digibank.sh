d8d7ab37ceaa3d59e85d3f9ca74fc10b2e5931a0
Matthew B. White <whitemat@uk.ibm.com>
2019-07-25 14:07:28 +0100

    [FAB-15213] Update Commercial Paper for Java
    Update with Java Contract
    
    Change-Id: I1a70473f038576d741c8c9ac83e527b5e51e88ca
    Signed-off-by: Matthew B. White <whitemat@uk.ibm.com>
    
    [FAB-15213] Update Commercial Paper for Java
    
    Change-Id: Ie116962a6c3952e52269323492f5f292e9b65e95
    Signed-off-by: Matthew B. White <whitemat@uk.ibm.com>

---
+++ commercial-paper/roles/digibank.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+function _exit(){
+    printf "Exiting:%s\n" "$1"
+    exit -1
+}
+
+(return 0 2>/dev/null) || _exit "Please run source i.e.  'source ./digibank.sh'"
+
+# Where am I?
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+
+cd "${DIR}/organization/digibank/configuration/cli"
+docker-compose -f docker-compose.yml up -d cliDigiBank
+cd "${DIR}/organization/digibank/"
+set -v
+#
+# Install and Instantiate a Smart Contract as 'Magnetocorp'
+#
+# 
+# Run Applications in either langauage (can be different from the Smart Contract)
+#
+# JavaScript Client Aplications:
+#
+# To add identity to the wallet:   node addToWallet.js
+#    < issue the paper run as Magentocorp>
+# To buy the paper             :   node buy.js
+# To redeem the paper          :   node redeem.js
+#
+# Java Client Applications:
+#
+# (remember to build the Java first with 'mvn clean package')
+#
+#    < issue the paper run as Magentocorp>
+# To buy the paper             :   node buy.js
+# To redeem the paper          :   node redeem.js
+#
+set +v
