c360dfc3b09bcc42d1fb1ffa6a2ced5c5c1c4989
David Enyeart <enyeart@us.ibm.com>
2019-08-23 17:56:32 -0400

    [FAB-16402] Fabcar only needs two endorsements
    Fabcar test currently invokes on 4 peers. Only need endorsement on 2 peers.
    This will help the CLI invokes to finish sooner, and eliminate some CI failures.
    
    Change-Id: Iec6bb7c9032ea6344c1d1917ffa3188c3b05c1e0
    Signed-off-by: David Enyeart <enyeart@us.ibm.com>

--- fabcar/startFabric.sh
+++ fabcar/startFabric.sh
@@ -66,36 +66,10 @@ docker exec \
     -p "$CC_SRC_PATH" \
     -l "$CC_RUNTIME_LANGUAGE"
 
-echo "Installing smart contract on peer1.org1.example.com"
-docker exec \
-  -e CORE_PEER_LOCALMSPID=Org1MSP \
-  -e CORE_PEER_ADDRESS=peer1.org1.example.com:8051 \
-  -e CORE_PEER_MSPCONFIGPATH=${ORG1_MSPCONFIGPATH} \
-  -e CORE_PEER_TLS_ROOTCERT_FILE=${ORG1_TLS_ROOTCERT_FILE} \
-  cli \
-  peer chaincode install \
-    -n fabcar \
-    -v 1.0 \
-    -p "$CC_SRC_PATH" \
-    -l "$CC_RUNTIME_LANGUAGE"
-
 echo "Installing smart contract on peer0.org2.example.com"
 docker exec \
   -e CORE_PEER_LOCALMSPID=Org2MSP \
   -e CORE_PEER_ADDRESS=peer0.org2.example.com:9051 \
-  -e CORE_PEER_MSPCONFIGPATH=${ORG2_MSPCONFIGPATH} \
-  -e CORE_PEER_TLS_ROOTCERT_FILE=${ORG2_TLS_ROOTCERT_FILE} \
-  cli \
-  peer chaincode install \
-    -n fabcar \
-    -v 1.0 \
-    -p "$CC_SRC_PATH" \
-    -l "$CC_RUNTIME_LANGUAGE"
-
-echo "Installing smart contract on peer1.org2.example.com"
-docker exec \
-  -e CORE_PEER_LOCALMSPID=Org2MSP \
-  -e CORE_PEER_ADDRESS=peer1.org2.example.com:10051 \
   -e CORE_PEER_MSPCONFIGPATH=${ORG2_MSPCONFIGPATH} \
   -e CORE_PEER_TLS_ROOTCERT_FILE=${ORG2_TLS_ROOTCERT_FILE} \
   cli \
@@ -141,12 +115,8 @@ docker exec \
     --tls \
     --cafile ${ORDERER_TLS_ROOTCERT_FILE} \
     --peerAddresses peer0.org1.example.com:7051 \
-    --peerAddresses peer1.org1.example.com:8051 \
     --peerAddresses peer0.org2.example.com:9051 \
-    --peerAddresses peer1.org2.example.com:10051 \
     --tlsRootCertFiles ${ORG1_TLS_ROOTCERT_FILE} \
-    --tlsRootCertFiles ${ORG1_TLS_ROOTCERT_FILE} \
-    --tlsRootCertFiles ${ORG2_TLS_ROOTCERT_FILE} \
     --tlsRootCertFiles ${ORG2_TLS_ROOTCERT_FILE}
 set +x
 
